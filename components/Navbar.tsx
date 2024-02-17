f6d8d34ad1fb2521fc0973536810d21acba95a44
Mohammad Faizan <faizancurious@gmail.com>
2024-02-18 00:20:13 +0530

    Minor Changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,8 @@
+"use client"
+
+import { Coffee, QrCode, Share2 } from 'lucide-react';
 import Link from 'next/link';
+import { Button } from './ui/button';
 import { Separator } from './ui/separator';
 
 const navItems = {
@@ -19,6 +23,11 @@ const navItems = {
     },
 };
 
+const copyURL = () => {
+    const url = window.location.href;
+    navigator.clipboard.writeText(url);
+};
+
 export function Navbar() {
     return (
         <aside className="-ml-[8px] mb-16 tracking-tight">
@@ -27,7 +36,7 @@ export function Navbar() {
                     className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
                     id="nav"
                 >
-                    <div className="flex flex-row">
+                    <div className="flex flex-row items-center">
                         {Object.entries(navItems).map(([path, { name }]) => {
                             return (
                                 <div className='flex flex-row items-center '>
@@ -38,11 +47,17 @@ export function Navbar() {
                                     >
                                         {name}
                                     </Link>
-                                    <Separator orientation="vertical" className='w-[1px] h-5 mx-3 items-center justify-center bg-[#252525]'/>
+                                    <Separator orientation="vertical" className='w-[1px] h-5 mx-3 items-center justify-center bg-[#252525]' />
                                 </div>
 
                             );
                         })}
+                        <Link href={'https://github.com/sponsors/hellofaizan'} className='flex flex-row items-center'>
+                            <Coffee className='hover:text-yellow-400 p-[2px] rounded' />
+                        </Link>
+                        <Button variant={'ghost'} size={'icon'} className='flex flex-row items-center' onClick={() => {copyURL()}}>
+                            <Share2 className='hover:text-green-400 p-[2px] rounded' />
+                        </Button>
                     </div>
                 </nav>
             </div>
