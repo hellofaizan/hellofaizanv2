992957009fe1331eb72c07000aa9babd4b664754
Mohammad Faizan <faizancurious@gmail.com>
2024-02-19 18:27:06 +0530

    robots page

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots() {
           userAgent: '*',
         },
       ],
-      sitemap: 'https://leerob.io/sitemap.xml',
-      host: 'https://leerob.io',
+      sitemap: 'https://hellofaizan.tech/sitemap.xml',
+      host: 'https://hellofaizan.tech',
     };
   }  
