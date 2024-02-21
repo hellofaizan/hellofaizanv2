7b544ad05816c6fb5f50ca100bfafcb0b3c379d3
Mohammad Faizan <faizancurious@gmail.com>
2024-02-21 17:49:45 +0530

    fixed nav bar alignment

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,44 +29,40 @@ const navItems = {
 
 export function Navbar() {
     return (
-        <aside className="mb-14">
-            <div className="lg:sticky lg:top-20">
-                <nav
-                    className="flex flex-row items-start px-0 pb-0 overflow-auto">
+        <nav
+            className="flex flex-row sticky mb-16">
 
-                    <div className='flex items-center justify-between w-full text-lg absolute'>
-                        <div className="flex flex-row items-center">
-                            {Object.entries(navItems).map(([path, { name }]) => {
-                                return (
-                                    <div className='flex flex-row items-center '>
-                                        <Link
-                                            key={path}
-                                            href={path}
-                                            className="transition-all hover:text-neutral-200 hover:bg-gray-500/10 rounded-md flex align-middle relative px-2"
-                                        >
-                                            {name}
-                                        </Link>
-                                        <Separator orientation="vertical" className='w-[1px] h-5 mx-1 items-center justify-center bg-[#696969]' />
-                                    </div>
+            <div className='flex items-center justify-between text-lg absolute w-full'>
+                <div className="flex flex-row items-center flex-1">
+                    {Object.entries(navItems).map(([path, { name }]) => {
+                        return (
+                            <div className='flex flex-row items-center '>
+                                <Link
+                                    key={path}
+                                    href={path}
+                                    className="transition-all hover:text-neutral-200 hover:bg-gray-500/10 rounded-md flex align-middle px-2"
+                                >
+                                    {name}
+                                </Link>
+                                <Separator orientation="vertical" className='w-[1px] h-5 mx-1 items-center justify-center bg-[#696969]' />
+                            </div>
 
-                                );
-                            })}
+                        );
+                    })}
 
-                            <Navigation />
+                    <Navigation />
 
-                            <Dono />
+                    <Dono />
 
-                            <QRCode />
-                        </div>
+                    <QRCode />
+                </div>
 
-                        <div>
-                            <Button variant={'ghost'} size={'icon'} className='flex-row items-center flex hover:bg-gray-500/5'>
-                                <Menu className='rounded' />
-                            </Button>
-                        </div>
-                    </div>
-                </nav>
+                <div>
+                    <Button variant={'ghost'} size={'icon'} className='flex-row items-center flex hover:bg-gray-500/5'>
+                        <Menu className='rounded' />
+                    </Button>
+                </div>
             </div>
-        </aside>
+        </nav>
     );
 }
